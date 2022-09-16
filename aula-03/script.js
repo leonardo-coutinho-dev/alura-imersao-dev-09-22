4fd9732e6dd8b027f377df51efd0334af15f9ec5
leonardo <web.dev.trabalho@gmail.com>
2022-09-16 08:54:25 -0300

    fazendo a terceira aplicação

--- aula-03/script.js
+++ aula-03/script.js
@@ -1,14 +1,24 @@
+let counter = 0;
+
 const numeroAleatorio = parseInt(Math.random() * 11);
+
+const gerarNovoNumeroAleatorio = () => {
+  window.location.reload();
+};
+
 const elementoResposta = document.getElementById("texto-resposta");
+const elementoTentativas = document.getElementById("numero-de-tentativas");
 
 const onClick = () => {
   const numeroEntrada = document.getElementById("valor").value;
   if (numeroEntrada == numeroAleatorio) {
-    elementoResposta.innerHTML = `Parabéns, você acertou o número, e esse número é ${numeroAleatorio}!`
+    elementoResposta.innerHTML = `Parabéns, você acertou o número, e esse número é ${numeroAleatorio}! Você acertou na ${counter + 1}ª tentativa!`;
+    counter = 0;
   } else if (numeroEntrada < 0 || numeroEntrada > 10) {
-    elementoResposta.innerHTML = `Por favor, digite um número entre 0 e 10 para continuar!`
+    elementoResposta.innerHTML = `Por favor, digite um número entre 0 e 10 para continuar!`;
   } else {
-    elementoResposta.innerHTML = `Você errou, tente novamente, dessa vez com outro número!`
+    counter++;
+    elementoTentativas.innerHTML = `O número de tentativas é: ${counter}!`
+    elementoResposta.innerHTML = `Você errou, tente novamente, dessa vez com outro número!`;
   }
-}
-
+};
